328350d363338652e36ff3fa146a7690a808b7d3
Ákos Pap <papkos@gmail.com>
2016-03-09 14:39:41 +0100

    Minesweeper cleanup (v1).

--- src/Minesweeper.java
+++ src/Minesweeper.java
@@ -2,84 +2,72 @@ import java.util.Scanner;
 
 public class Minesweeper {
 
-	private static MineField field;
 	private static Ranking rank;
+
+	private static boolean continueGame = true;
 
 	public static void main(String[] args) {
 		rank = new Ranking();
 		mainMessage();
 
-		while (gameCountinue()) {
-			;
+		while (continueGame) {
+			gameLoop();
 		}
+
 		System.out.println("\nThank you for playing :) Have a nice day!");
 	}
 
-	private static boolean gameCountinue() {
-		field = new MineField();
+	private static void gameLoop() {
+		MineField field = new MineField();
 		int result = 0;
-		while (true) {
+		boolean playing = true;
 
+		while (playing) {
 			field.show();
-			System.out.print("\nPlease enter your move(row col): ");
+			System.out.print("\nPlease enter your move (row col): ");
 			Scanner in = new Scanner(System.in);
 			String input = in.nextLine();
 
-			if (input.equals("top")) {
-				rank.show();
-				continue;
-			}
-
-			if (input.equals("restart")) {
-				rank.recordName(result);
-				return true;
-			}
-
-			if (input.equals("exit")) {
-				rank.recordName(result);
-				return false;
-			}
-
-			if (field.legalMoveString(input)) {
-				result++;
-				if (result == 35) {
-					System.out.println("Congratulations you WON the game!");
+			switch (input) {
+				case "top":
+					rank.show();
+					break;
+				case "restart":
 					rank.recordName(result);
-					return true;
+					playing = false;
+					break;
+				case "exit":
+					rank.recordName(result);
+					playing = false;
+					continueGame = false;
+					break;
+				default: {
+					if (field.legalMoveString(input)) {
+						result++;
+						if (result == 35) {
+							System.out.println("Congratulations you WON the game!");
+							rank.recordName(result);
+							playing = false;
+						}
+					} else if (field.getBoom()) {
+						System.out.println("\nBooooooooooooooooooooooooooooom! You stepped on a mine! " +
+								"You survived " + result + " turns");
+						rank.recordName(result);
+						playing = false;
+					}
+					break;
 				}
-				continue;
 			}
-//			if(input.equals("exit")){
-//				rank.recordName(result);
-//				return false;
-//			}
-//			if(field.legalMoveString(input)){
-//				result++;
-//				if(result==35){
-//					System.out.println("Congratulations you WON the game!");
-//					rank.recordName(result);
-//					return true;
-//				}
-//				continue;
-//			}
-			else if (field.getBoom()) {
-				System.out.println(
-						"\nBooooooooooooooooooooooooooooom! You stepped on a mine!You survived " + result + " turns");
-				rank.recordName(result);
-				return true;
-			}
-
 		}
-
 	}
 
 	private static void mainMessage() {
 		System.out.println("Welcome to Minesweeper!");
-		System.out.println("To play just input some coordinates and try not to step ont mine :)");
-		System.out.println("Usefull commands:");
-		System.out.println("restart- Starts a new game.");
-		System.out.println("exit- Quits the game.");
-		System.out.println("top- Reveals the top scoreboard.");
+		System.out.println("To play just input some coordinates and try not to step on a mine :)");
+		System.out.println("Useful commands:");
+		System.out.println("restart - Starts a new game.");
+		System.out.println("exit - Quits the game.");
+		System.out.println("top - Reveals the top scoreboard.");
 		System.out.println("Have Fun !");
 	}
 }
